a4e7087766f73d8ffb61f28ad72e463ad46d26dc
bharath <bharath4657@gmail.com>
2023-12-28 16:00:39 +0530

    condition_10

--- conditions.sh
+++ conditions.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
-
+#!/bin/bash 
 arch=$(uname -m)
 
 if [ "$arch" == "x86_64" ]; then
-    echo "64-bit system"
-else
-    echo "32-bit system"
+    echo "64-bit"
+else 
+    echo "32-bit"
 fi
+    

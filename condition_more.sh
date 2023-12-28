be5205bfeafae2fdfe2dfb86acc0832384b808fb
bharath <bharath4657@gmail.com>
2023-12-28 16:09:44 +0530

    condition_13

--- condition_more.sh
+++ condition_more.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 version=$(cat /etc/redhat-release)
 
-if [ "$version" == "CentOS Stream release 8"]; then
+if [ "$version" == "CentOS Stream release 8" ]; then
     echo "centos machine"
 else 
     echo "redhat machine"

6fea05893b24aa8aa83d9b772feabf74053d0ce1
bharath <bharath4657@gmail.com>
2023-12-30 00:10:56 +0530

    dsafssadsdaasd

---
+++ withfunction.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+ID=$(id -u)
+
+VALIDATE()
+RULE=$(echo $?)
+echo "output is $RULE"
+if [ $1 -ne 0 ]
+then
+echo "error: installation is failed"
+exit 1
+else 
+echo "installation is successful"
+fi
+
+
+if [ $ID -ne 0 ]
+then
+echo "error: script will run with root previlages"
+exit 1
+else 
+echo "you are the root user"
+fi   #it exits
+yum install mysql -y
+
+VALIDATE $? "installation successfull"
+
+yum install git 
+
+VALIDATE $? "installation completed"
+
+
+
+
